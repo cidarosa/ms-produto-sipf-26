107340711317077cdf18353c15d343a6d26e7fc5
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-04 09:45:52 -0300

    Exceptions handler

--- src/main/java/com/github/cidarosa/ms/produto/controller/ProdutoController.java
+++ src/main/java/com/github/cidarosa/ms/produto/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.github.cidarosa.ms.produto.dto.ProdutoDTO;
 import com.github.cidarosa.ms.produto.service.ProdutoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,12 @@ public class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
+
+    @Profile("test")
+    @GetMapping("/--demo/500")
+    public String force500(){
+        throw new RuntimeException("Erro 500 forçado para demonstração");
+    }
 
     @GetMapping
     public ResponseEntity<List<ProdutoDTO>> getAllProdutos() {
---
+++ src/main/java/com/github/cidarosa/ms/produto/exceptions/dto/FielMessageDTO.java
@@ -0,0 +1,15 @@
+package com.github.cidarosa.ms.produto.exceptions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class FielMessageDTO {
+
+    private String fieldName;
+    private String message;
+
+}
